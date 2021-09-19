57adf4bc669c7a7a9498b46c703498fd8307ffdd
sun pochin <sunpochin@gmail.com>
2021-09-19 23:23:12 +0800

    adding gravatar

--- application.py
+++ application.py
@@ -9,11 +9,15 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
-
 from flask_ckeditor import CKEditor
+
 
 application = Flask(__name__)
 ckeditor = CKEditor(application)
+
+gravatar = Gravatar(application, size=100, rating='g', default='retro',
+                    force_default=False, force_lower=False, use_ssl=False, base_url=None)
+
 
 application.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(application)
